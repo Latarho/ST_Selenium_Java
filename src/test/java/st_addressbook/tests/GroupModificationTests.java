df54f537642c56000500c7379bc2321000509599
latarho <latarho2@gmail.com>
2021-10-30 17:23:38 +0300

    Создан автотест - модификация выбранной группы.

---
+++ src/test/java/st_addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,19 @@
+package st_addressbook.tests;
+
+import org.junit.Test;
+import st_addressbook.model.GroupData;
+
+
+public class GroupModificationTests extends TestBase{
+
+    @Test
+    public void testGroupCreation() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test4", "test5", "test6"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+
+}
